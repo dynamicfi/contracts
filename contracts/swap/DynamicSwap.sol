b8a4a0d91de22d7f60cc959686cd7c0688a20c00
trieult <amhanhnguxu@gmail.com>
2022-07-06 08:00:04 +0700

    update swap

--- contracts/swap/DynamicSwap.sol
+++ contracts/swap/DynamicSwap.sol
@@ -3,95 +3,104 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./UniswapInterface.sol";
 
-contract DynamicSwap {
+contract DynamicSwap is Ownable {
     using SafeERC20 for IERC20;
     // address private constant UNISWAP_V2_ROUTER =
     //     0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     // address private immutable WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address private immutable WETH;
     address private immutable ROUTER;
+    uint256 private _fee;
 
-    constructor(address router_, address weth_) {
+    constructor(
+        address router_,
+        address weth_,
+        uint256 fee_
+    ) {
         ROUTER = router_;
         WETH = weth_;
+        _fee = fee_;
     }
 
     receive() external payable {}
 
     function swapTokenForToken(
-        address _tokenIn,
-        address _tokenOut,
-        uint256 _amountIn,
-        uint256 _amountOutMin,
-        address _to
+        address tokenIn_,
+        address tokenOut_,
+        uint256 amountIn_,
+        uint256 amountOutMin_,
+        address to_,
+        uint256 deadline_
     ) external {
-        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
-        IERC20(_tokenIn).approve(ROUTER, _amountIn);
+        IERC20(tokenIn_).transferFrom(msg.sender, address(this), amountIn_);
+        IERC20(tokenIn_).approve(ROUTER, amountIn_);
 
         address[] memory path;
-        if (_tokenIn == WETH || _tokenOut == WETH) {
+        if (tokenIn_ == WETH || tokenOut_ == WETH) {
             path = new address[](2);
-            path[0] = _tokenIn;
-            path[1] = _tokenOut;
+            path[0] = tokenIn_;
+            path[1] = tokenOut_;
         } else {
             path = new address[](3);
-            path[0] = _tokenIn;
+            path[0] = tokenIn_;
             path[1] = WETH;
-            path[2] = _tokenOut;
+            path[2] = tokenOut_;
         }
 
         IUniswapV2Router(ROUTER).swapExactTokensForTokens(
-            _amountIn,
-            _amountOutMin,
+            amountIn_,
+            amountOutMin_,
             path,
-            _to,
-            block.timestamp
+            to_,
+            deadline_
         );
     }
 
     function swapEthForToken(
-        address _router,
-        address _tokenOut,
-        uint256 _amountOutMin,
-        address _to
+        uint256 amountOutMin_,
+        address[] calldata path_,
+        address to_,
+        uint256 deadline_
     ) external payable {
-        address[] memory path;
-        path[0] = _tokenOut;
-
-        (bool success, ) = _router.call{value: msg.value}(
+        (bool success, ) = ROUTER.call{value: msg.value}(
             abi.encodeWithSignature(
                 "swapExactETHForTokens(uint256 amountOutMin,address[] calldata path,address to,uint256 deadline)",
-                _amountOutMin,
-                path,
-                _to,
-                block.timestamp
+                amountOutMin_,
+                path_,
+                to_,
+                deadline_
             )
         );
         require(success, "transaction failed");
     }
 
     function swapTokenForEth(
-        address _router,
-        address _tokenIn,
-        uint256 _amountIn,
-        uint256 _amountOutMin,
-        address _to
-    ) external payable {
-        address[] memory path;
-        path[0] = _tokenIn;
-
-        (bool success, ) = _router.call{value: msg.value}(
+        uint256 amountIn_,
+        uint256 amountOutMin_,
+        address[] calldata path_,
+        address to_,
+        uint256 deadline_
+    ) external {
+        (bool success, ) = ROUTER.call(
             abi.encodeWithSignature(
                 "swapExactTokensForETH(uint256 amountIn,uint256 amountOutMin,address[] calldata path,address to,uint256 deadline)",
-                _amountIn,
-                _amountOutMin,
-                path,
-                _to,
-                block.timestamp
+                amountIn_,
+                amountOutMin_,
+                path_,
+                to_,
+                deadline_
             )
         );
         require(success, "transaction failed");
+    }
+
+    function claimToken(address token_, address receipt_) public onlyOwner {
+        IERC20(token_).safeTransfer(
+            receipt_,
+            IERC20(token_).balanceOf(address(this))
+        );
     }
 }
