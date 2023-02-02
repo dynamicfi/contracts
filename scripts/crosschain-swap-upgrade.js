49786c4fd5535040e1c22965f07df24f4bcd4b49
TonyDao <tuanda1@vmodev.com>
2023-02-03 00:23:45 +0700

    add script upgrade swap

---
+++ scripts/crosschain-swap-upgrade.js
@@ -0,0 +1,31 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+const { upgrades } = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const SWAP_CONTRACT_ADDRESS = "0x4abccdd52E3BCbEE3C5Df743B4f82FEbC860Ac63";
+
+  const CrossChain = await hre.ethers.getContractFactory("DyBEP20Venus");
+  await upgrades.upgradeProxy(SWAP_CONTRACT_ADDRESS, CrossChain);
+
+  console.log("CrossChain upgraded successfully");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
